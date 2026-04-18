0342295543a9fe64fadffb2ce1684ce0b78c7b12
sketlou <72133764+sketlou@users.noreply.github.com>
2026-04-18 21:54:28 +0300

    Update main.py asdasdsaasdadss

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ import asyncio
 import json
 import logging
 import random
+import os
 from collections import defaultdict
 from datetime import datetime, timedelta, timezone
 from pathlib import Path
-
+from telegram import Bot
+from telegram.constants import ParseMode
+from telegram.ext import DefaultBotProperties
 from aiogram import Bot, Dispatcher, F
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode, ChatMemberStatus
@@ -25,6 +28,11 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 ADMIN_CHAT_ID = os.getenv("ADMIN_CHAT_ID")
 CHANNEL_USERNAME = os.getenv("CHANNEL_USERNAME")
 CHANNEL_ID = os.getenv("CHANNEL_ID")
+
+bot = Bot(
+    token=BOT_TOKEN,
+    default=DefaultBotProperties(parse_mode=ParseMode.HTML)
+)
 
 YOUTUBE_CHANNEL_URL = "https://www.youtube.com/@Kaylor5rp"
 LAST_VIDEO_URL = "https://youtu.be/EYmKJhSDIgg?si=MOO0dvzvPCWCirj5"
